500497243e0cba655d46291249fbe1457fd401f9
Dean Anderson <DeanMAnderson@hotmail.co.uk>
2018-12-18 22:59:58 +0000

    Add HomePage and HomePageTest
    Added home page GoTo and IsAt test based on page title.

---
+++ SeleniumFramework/Pages/HomePage.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeleniumFramework.Pages
+{
+    public static class HomePage
+    {
+        private static string Url => "http://www.google.co.uk";
+
+        public static void GoTo()
+        {
+            Driver.Instance.Navigate().GoToUrl(Url);
+        }
+
+        public static bool IsAt()
+        {
+            return Driver.Instance.Title == "Google";
+        }
+    }
+}
---
+++ SeleniumFrameworkTest/Pages/HomePageTest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SeleniumFramework.Pages;
+
+namespace SeleniumFrameworkTest.Pages
+{
+    [TestClass]
+    public class HomePageTest
+    {
+        [TestMethod]
+        public void CanGoToAndIsAt_True()
+        {
+            HomePage.GoTo();
+
+            Assert.IsTrue(HomePage.IsAt());
+        }
+    }
+}
