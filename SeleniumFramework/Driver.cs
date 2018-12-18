6d975783bf863e13e2649fed9070bf500f9af2fa
Dean Anderson <DeanMAnderson@hotmail.co.uk>
2018-12-18 22:39:18 +0000

    Add Driver Singleton
    Simple singleton instance for single web engine access.

---
+++ SeleniumFramework/Driver.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+
+namespace SeleniumFramework
+{
+    public class Driver
+    {
+        internal static IWebDriver Instance { get; private set; } = new FirefoxDriver();
+    }
+}
